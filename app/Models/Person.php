0ad74da5c752d4c3b4f9ba9617704f0658e4a92e
jakemackie <jake.wmackie@gmail.com>
2024-11-09 13:31:09 +0000

    feature: Database migrations and models

---
+++ app/Models/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $fillable = [
+        'name',
+        'age',
+        'bio'
+    ];
+
+    /** @use HasFactory<\Database\Factories\PersonFactory> */
+    use HasFactory;
+}
