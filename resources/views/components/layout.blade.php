d2e4861f53b4a7b270b6ad9a708e250996cdecbf
jakemackie <jake.wmackie@gmail.com>
2024-11-11 21:55:58 +0000

    amend: Styling changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,8 +12,8 @@
 <body>
 
     <header class="py-8 bg-white shadow-sm">
-        <nav class="mx-auto max-w-screen-lg flex justify-between text-xl font-semibold">
-            <a href="/" class="text-2xl">My project</a>
+        <nav class="mx-auto px-4 lg:px-0 max-w-screen-lg flex justify-between text-lg lg:text-xl font-semibold">
+            <a href="/" class="text-xl lg:text-2xl">My project</a>
             <div class="flex gap-8">
                 <a href="{{ route('people.index') }}">All people</a>
                 <a href="{{ route('people.create') }}">Create person</a>
