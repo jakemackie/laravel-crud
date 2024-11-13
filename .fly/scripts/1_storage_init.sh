fbd8177a0eb8a1def0c6c1bd05e9044c90b1c5e4
Jake Mackie <jake@hiyield.co.uk>
2024-11-13 14:41:53 +0000

    feat: Fly prod changes

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -1,9 +1,7 @@
-# .fly/scripts/1_storage_init.sh
-
-# Add this below the storage folder initialization snippet
-FOLDER=/var/www/html/storage/database
+FOLDER=/var/www/html/storage/app
 if [ ! -d "$FOLDER" ]; then
-    echo "$FOLDER is not a directory, initializing database" 
-    mkdir /var/www/html/storage/database
-    touch /var/www/html/storage/database/database.sqlite
+    echo "$FOLDER is not a directory, copying storage_ content to storage"
+    cp -r /var/www/html/storage_/. /var/www/html/storage
+    echo "deleting storage_..."
+    rm -rf /var/www/html/storage_
 fi
