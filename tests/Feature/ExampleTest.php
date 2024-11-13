c48b0f693ad5326ff92316dd26021002cacb7cc1
Jake Mackie <jake@hiyield.co.uk>
2024-11-13 01:57:51 +0000

    amend: Revert unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('GET / Returns a successful response.', function () {
+it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
